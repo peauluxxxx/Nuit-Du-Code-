51208246215a5601aab571075d3d7f0fb860209e
peauluxxxx <paulduvivierporchet@gmail.com>
2025-05-16 17:11:04 +0700

    Update knight_of_darkness.py

--- knight_of_darkness.py
+++ knight_of_darkness.py
@@ -18,6 +18,12 @@ SAUT_VELOCITE = -5
 SOL_Y = 112  
 COLKEY = 2
 
+co_tuile_p =[(0,16),(16,16),(38,16),(48,16)]
+
+
+
+
+#fonctions
 def appliquer_gravite():
     personnage["vy"] += GRAVITE
     personnage["y"] += personnage["vy"]
@@ -42,14 +48,29 @@ def personnage_update():
         if personnage["y"] + personnage["hauteur"] >= SOL_Y:
             personnage["vy"] = SAUT_VELOCITE
 
+
+
+# animation
+def animation(spr,sprx,spry):
+    f = py.frame_count % len(spr)
+    py.blt(sprx, spry, 0, spr[f][0], spr[f][1],16,16, colkey = COLKEY)
+        
+
+
+
+
 def update():
     personnage_update()
     appliquer_gravite()
     gerer_collision_avec_sol()
+    animation(co_tuile_p, personnage["x"],personnage["y"])
+
+
 
 def draw():
+    global personnage
     py.cls(0)
     py.bltm(0, 0, 0, 0, 0, 128, 128)
-    py.blt(personnage["x"], personnage["y"], 0, 0, 16, 16, 16, colkey=COLKEY)
+    personnage = py.blt(personnage['x'],personnage['y'],0,0,16,16,16)
 
 py.run(update, draw)
