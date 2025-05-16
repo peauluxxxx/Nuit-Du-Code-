249100026dd5586db4ae88898e876e0dab8dbfce
Shoowan <showan.shorts@gmail.com>
2025-05-16 14:48:44 +0200

    Update knight_of_darkness.py

--- knight_of_darkness.py
+++ knight_of_darkness.py
@@ -1,47 +1,42 @@
 import pyxel as py
 
-
-# Initialisation de la fenêtre et chargement du fichier ressource.
 py.init(128, 128, title="Knight of Darkness NDC")
 py.load("2.pyxres")
 
-# Ajout de la musique 
-py.playm(0, loop = True)
-
-# Variable principale
+py.play(0, [0,0,0,0,0,0], loop = True)
+py.play(1, [1,1,2,1,1,3], loop = True)
 animation_frame = 0
 sprite_x = 0
 sprite_y = 16
 compteur = 0
 sprite_chevalier = [(0,16), (16,16), (32,16), (48,16)]
 coffre = {"x": 0,
-          "y": 16}
-coeur = 3
-
+          "y": 16
+          }
+coeur = 2
+pos_decor = 128
+ennemis = []
 
 # Constantes
 GRAVITE = 0.5
 SAUT_FORCE = -4
 SOL_Y = 112
 COLKEY = 2
-
+t = 0
+lvl2 = False
 # État du personnage
 personnage = {
     "x": 10,
     "y": 50,
-    "vx": 2,
+    "vx": 1,
     "vy": 0,
     "largeur": 16,
     "hauteur": 16,
     "en_saut": False
 }
 
-## FONCTION
 # Fonctions de collision
-
 def collision_verticale(y_offset):
-    """ fonction permettant la collision verticale du personnage avec le décor
-    """
     for i in range(personnage["largeur"]):
         couleur = py.pget(personnage["x"] + i, personnage["y"] + y_offset)
         if couleur == 0:
@@ -49,8 +44,6 @@ def collision_verticale(y_offset):
     return False
 
 def collision_horizontale(x_offset):
-    """ fonction permettant la collision horizontale du personnage avec le décor
-    """
     for i in range(personnage["hauteur"]):
         couleur = py.pget(personnage["x"] + x_offset, personnage["y"] + i)
         if couleur == 0:
@@ -58,15 +51,19 @@ def collision_horizontale(x_offset):
     return False
 
 # Mise à jour du personnage
-
 def personnage_update():
-    """ fonction permettant de déplacer et de faire sauter en appliquant la gravité et la collision plafond
-    """
+    global pos_decor
     # Déplacement horizontal
     if py.btn(py.KEY_Q) and not collision_horizontale(-1):
+        if lvl2 == True:
+            pos_decor -= 4
+            vx = 0
         personnage["x"] -= personnage["vx"]
         animation()
     if py.btn(py.KEY_D) and not collision_horizontale(personnage["largeur"]):
+        if lvl2 == True:
+            pos_decor += 4
+            vx = 0
         personnage["x"] += personnage["vx"]
         animation()
 
@@ -90,8 +87,6 @@ def personnage_update():
 
 
 def animation():
-    """ fonction permettant d'animer le personnage en changeant de sprite
-    """
     global sprite_x, sprite_y, animation_frame, compteur
     if animation_frame % 5 == 0:
         compteur += 1
@@ -102,22 +97,26 @@ def animation():
 
 
 def dessin_coeur(coeur):
-    """ fonction permettant de dessiner les coeurs en fonction de la valeur de l'integer dans la variable coeur
-    """
     x = 80
     for i in range(coeur):
         py.blt(x,0, 0,112, 48, 16, 16)
         x+= 16
 
 def dessin_coffre(coffre):
-    """ fonction permettant de dessiner le coffre tout en lançant la fin du niveau et l'apparition du niveau suivant
-    """
+    global t, lvl2
     py.blt(coffre["x"],coffre["y"], 0,32, 32, 16, 16)
-    #if coffre['x'] + personnage['largeur'] == personnage['x'] :
+    if coffre['x'] + personnage['largeur'] == personnage['x'] and coffre['y'] == personnage['y']:
+        py.blt(coffre["x"],coffre["y"], 0,48, 32, 16, 16)
+        while t < 121 :
+            t += 1
+            print(t)
+        if t  >= 120 :
+            lvl2 = True
+            print(lvl2)
+
+
         
-        
-        
-#  fonction UPDATE
+
 def update():
     global animation_frame
     animation_frame += 1
@@ -125,13 +124,16 @@ def update():
         animation_frame = 0
     personnage_update()
 
-# fonction DRAW
+    
 def draw():
+    global lvl2
     py.cls(0)
-    py.bltm(0, 0, 0, 0, 0, 128, 128)
-    py.blt(personnage["x"], personnage["y"], 0, sprite_x, sprite_y, 16, 16, colkey=COLKEY)
-    dessin_coffre(coffre)
-    dessin_coeur(coeur)
+    if lvl2 == False :
+        py.bltm(0, 0, 0, 0, 0, 128, 128)
+        dessin_coffre(coffre)
+    else:
+        py.bltm(0,0,0, pos_decor, 0, 128, 128)
 
-# lancement du jeu 
+    py.blt(personnage["x"], personnage["y"], 0, sprite_x, sprite_y, 16, 16, colkey=COLKEY)
+    dessin_coeur(coeur)
 py.run(update, draw)
