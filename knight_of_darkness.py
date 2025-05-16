872f21c286ae60cc574d69741310939ed9f09c7c
peauluxxxx <paulduvivierporchet@gmail.com>
2025-05-16 14:36:56 +0700

    Update knight_of_darkness.py

--- knight_of_darkness.py
+++ knight_of_darkness.py
@@ -1,7 +1,19 @@
 import pyxel as py
 
 py.init(128,128,"knight_of_darkness NDC")
+py.load("2.pyxres")
 
+# Variable principale
+personnage_position = {"x": 10, "y": 90 }
+
+
+
+
+# fonctions 
+
+def personnage(personnage_position):
+  
+  
 
 
 
